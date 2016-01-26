bf91e8193bb3ef7207c2387809b1e360851c6cc4
Tim Vink <tim.vink@ing.nl>
2016-01-26 11:57:05 +0100

    Add getAllLinks to main script

--- main.R
+++ main.R
@@ -10,6 +10,8 @@ library(rvest)
 library(stringr)
 library(dplyr)
 library(ggplot2)
+library(data.table)
+library(purrr)
 
 # Just in case there are proxy problems
 Sys.setenv(http_proxy="")
@@ -19,13 +21,19 @@ Sys.setenv(no_proxy="*")
 
 # Source our functions
 source("functions/read_storing_page.R")
+source("functions/getAllLinks.R")
+
+# Todo / issues
+# in getAllLinks, make sure to set i dynamicly to number of pages.
 
 #### Load data ####
 
-# todo
+pages <- getAllLinks()
+data <- rbindlist(lapply(pages[1:10], read_storing_page), use.names = T, fill = T) # Reads in all the pages.
+
 
 #### Analyse data ####
-
+data
 #..
   
   
