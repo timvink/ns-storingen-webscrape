74071a0cd42af101940a0caed6dbb7af34aa3d8b
Tim Vink <tim.vink@ing.nl>
2016-01-26 13:53:42 +0100

    update main.R structure

--- main.R
+++ main.R
@@ -12,6 +12,8 @@ library(dplyr)
 library(ggplot2)
 library(data.table)
 library(purrr)
+library(lubridate)
+library(leaflet)
 
 # Just in case there are proxy problems
 Sys.setenv(http_proxy="")
@@ -26,17 +28,18 @@ source("functions/getAllLinks.R")
 # Todo / issues
 # in getAllLinks, make sure to set i dynamicly to number of pages.
 
+
 #### Load data ####
 
-# Takes about ~ 30 minutes of punishing rijdendetreinen.nl/ 
+# Takes about ~ 15 minutes of punishing rijdendetreinen.nl/ 
 pages <- getAllLinks()
 system.time(data <- rbindlist(lapply(pages, read_storing_page), use.names = T, fill = T)) # Reads in all the pages.
 
 # Save to a temporary folder 
+write.csv(data[1:20], "data/head.csv", row.names=F)
 write.csv(data, "data/data.csv", row.names=F)
 
 #### Analyse data ####
 
 #..
-  
   
