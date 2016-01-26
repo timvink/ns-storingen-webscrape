d5d8ec6bd3bd6ae95e9e43b50f77c33215f53576
chris <chris@chris.local>
2016-01-26 15:30:10 +0100

    updated functions and added mapping function

--- getLatLon.R
+++ getLatLon.R
@@ -9,7 +9,7 @@ library(data.table)
 
 getLatLon = function(data,stations){
   # make date column date type
-  #data$date = as.Date(data$date)
+  data$date = as.Date(data$date)
   
   # get origin locations
   setkey(data,origin)
@@ -17,15 +17,17 @@ getLatLon = function(data,stations){
   
   # join
   data2 = stations[,list('origin_x' = X,'origin_y' = Y,NAME)][data]
+  setnames(data2, "NAME" , "origin") #data2$origin = data2$NAME
   
   # get destination locations
   setkey(data2,destination)
   setkey(stations,NAME)
   data3 = stations[,list('dest_x' = X,'dest_y' = Y,NAME)][data2]
+  setnames(data3, "NAME" , "destination")
   
   # remove NAME columns
-  set(data3,j='NAME',value=NULL)
-  set(data3,j='i.NAME',value=NULL)
+  #set(data3,j='NAME',value=NULL)
+  #set(data3,j='i.NAME',value=NULL)
   # update all
   #data3[,!(colnames(data3) %in% 'storing'),with=F]
   return(data3)
